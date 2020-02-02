9c899da213deeccbf7ccfec2865595cc6b43ae7d
erictilford <60585158+erictilford@users.noreply.github.com>
2020-02-02 16:05:07 -0600

    changed to my doc's key

--- tableSnippet.js
+++ tableSnippet.js
@@ -1,5 +1,5 @@
 function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1kHpjMZl3TVcLbt_eNIu0k77wfSInQFHScgt5vDm51TE/pubhtml',
 callback: function(data, tabletop) {
 console.log(data)
 },
