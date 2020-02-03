8a0d7872c3a51999ce42367919e664d400070be9
erictilford <60585158+erictilford@users.noreply.github.com>
2020-02-02 19:01:15 -0600

    Delete tableSnippet.js

--- tableSnippet.js
+++
@@ -1,8 +0,0 @@
-function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1kHpjMZl3TVcLbt_eNIu0k77wfSInQFHScgt5vDm51TE/pubhtml',
-callback: function(data, tabletop) {
-console.log(data)
-},
-simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
