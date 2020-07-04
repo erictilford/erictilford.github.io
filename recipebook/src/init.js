7e089e58710f5a7ab9dc3670d0364a3feda69a56
erictilford <erictilford@gmail.com>
2020-07-03 23:02:43 -0500

    added unique highlight color to dark mode

--- recipebook/src/init.js
+++ recipebook/src/init.js
@@ -216,6 +216,8 @@ function showInfo(data) {
                 .setProperty('--tertiary-color', 'var(--light-tertiary)');
             document.documentElement.style
                 .setProperty('--text-color', 'var(--light-text)');
+            document.documentElement.style
+                .setProperty('--card-highlight', 'var(--light-card-highlight)');
             document.getElementsByClassName("light-mode-button")[0].className = "fa fa-moon-o fa-lg light-mode-button header-icon";
             darkMode = false;
         }
@@ -228,6 +230,8 @@ function showInfo(data) {
                 .setProperty('--tertiary-color', 'var(--dark-tertiary)');
             document.documentElement.style
                 .setProperty('--text-color', 'var(--dark-text)');
+            document.documentElement.style
+                .setProperty('--card-highlight', 'var(--dark-card-highlight)');
             document.getElementsByClassName("light-mode-button")[0].className = "fa fa-sun fa-lg light-mode-button header-icon";
             darkMode = true;
         }
