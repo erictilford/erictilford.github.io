9e15061313adf2e8894cb3b05bd873120d07f101
erictilford <erictilford@gmail.com>
2022-05-12 22:36:04 -0500

    added more wallpapers, moved list to config

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
     
     // WALLPAPERS
-    const wallpapers = [
-        "6zwch8y1q4nz.jpg",
-        "38bbDQM.jpg",
-        "d2fd98f2a2dcb000de2c971a69879eff.jpg",
-        "kcf4mtbjca231.jpg",
-        "lxkpopzwbgq21.png",
-        "nao5txxljprx.jpg",
-        "T5fmI19.jpg",
-        "X2qDwdN.jpg",
-        "xygoflg84kf21.png"
-    ];
     let randomWallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
     document.body.style.backgroundImage = "url('assets/backgrounds/"+ randomWallpaper +"')";
 
