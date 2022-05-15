2e1dd673ff7b0ab0fc7b26b3391628cfc7213d68
eric.tilford <eric.tilford@egov.com>
2022-05-15 15:47:29 -0500

    re-added uvi #

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -112,7 +112,7 @@ $(document).ready(function () {
 			else if (uvi >= 8 && uvi < 11) { uvtext = "Very High"; uvcolor = "crimson" }
 			else if (uvi >= 11) { uvtext = "Extreme"; uvcolor = "magenta" }
 
-			$("#weather-extra-uv").html('<i class="fa-solid fa-lg ' + uvicon + ' weather-extra-icon" style="color:' + uvcolor + '"></i><br>UV Index<br>' + uvtext /*+ " | " + result.current.uvi */);
+			$("#weather-extra-uv").html('<i class="fa-solid fa-lg ' + uvicon + ' weather-extra-icon" style="color:' + uvcolor + '"></i><br>UV Index<br>' + uvtext + " | " + result.current.uvi );
 			$("#weather-extra-wind").html('<i class="fa-solid fa-lg fa-wind weather-extra-icon" style="color:lightsteelblue"></i><br>Wind<br>' + result.current.wind_speed + ' mph');
 			$("#weather-extra-humidity").html('<i class="fa-solid fa-lg fa-droplet weather-extra-icon" style="color:cornflowerblue"></i><br>Humidity<br>' + result.current.humidity + '%');
 		},
