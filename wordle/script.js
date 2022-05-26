efb1fd28aee3f6f787bb94a84335bf7da0466457
eric.tilford <eric.tilford@egov.com>
2022-05-26 13:12:47 -0500

    added white square support

--- wordle/script.js
+++ wordle/script.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
     let bs = $("#bsquare").val();
     let ys = $("#ysquare").val();
     let gs = $("#gsquare").val();
-    let output = input.replace(/\u2B1B/g,bs).replace(/\uD83D\uDFE8|\uD83D\uDFE6/g,ys).replace(/\uD83D\uDFE9|\uD83D\uDFE7/g,gs); // https://emojiterra.com/
+    let output = input.replace(/\u2B1B|\u2B1C/g,bs).replace(/\uD83D\uDFE8|\uD83D\uDFE6/g,ys).replace(/\uD83D\uDFE9|\uD83D\uDFE7/g,gs); // https://emojiterra.com/
     $("#outputText").val(output);
     $("#outputDiv").show();
   });
