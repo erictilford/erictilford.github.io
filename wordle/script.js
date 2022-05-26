0f541cbdb1d4cd1792f9b7cdcdb5fa91725c72e1
erictilford <erictilford@gmail.com>
2022-05-25 22:23:59 -0500

    testing

--- wordle/script.js
+++ wordle/script.js
@@ -1,12 +1,16 @@
 $( document ).ready(function() {
   $("#convert-button").click(function() {
-    let input = $("#input").val();
+    let input = $("#inputText").val();
+    console.log("input = " + input);
     let bs = $("#bsquare").val();
     let ys = $("#ysquare").val();
-    let gs = $("#ysquare").val();
+    let gs = $("#gsquare").val();
     //let str = input.toLowerCase();
-    let output = input.replace(/:black_large_square:/g,bs).replace(/:large_yellow_square:/g,ys).replace(/:large_green_square:/g,gs);
-    console.log(bs);
-    //$("#outputText").text(input);
+    let output = input.replace(/:black_large_square:/g,bs).replace(/:large_yellow_square:/g,ys).replace(/:large_green_square:/g,"geese");
+    console.log("bs = " + bs);
+    console.log("gs = " + gs);
+    console.log("ys = " + ys);
+    console.log(output);
+    $("#outputText").val(output);
   });
 });
