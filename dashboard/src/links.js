4bcfe94b936ed29e299e91d23d899de64a3645ff
eric.tilford <eric.tilford@egov.com>
2022-05-30 22:37:44 -0500

    added links.js

---
+++ dashboard/src/links.js
@@ -0,0 +1,91 @@
+const links = [
+    {
+        "title" : "Toonami Aftermath",
+        "url" : "https://www.toonamiaftermath.com",
+        "icon" : '<i class="fa-solid fa-2x fa-meteor" style="color:mediumaquamarine"></i>',
+        "target" : "#video-list"
+    },
+    {
+        "title" : "Twitch",
+        "url" : "https://www.twitch.tv/directory/following/live",
+        "icon" : '<i class="fa-brands fa-2x fa-twitch" style="color:#a970ff"></i>',
+        "target" : "#video-list"
+    },
+    {
+        "title" : "YouTube",
+        "url" : "https://www.youtube.com",
+        "icon" : '<i class="fa-brands fa-2x fa-youtube" style="color:red;"></i>',
+        "target" : "#video-list"
+    },
+    {
+        "title" : "Netflix",
+        "url" : "https://www.netflix.com",
+        "icon" : '<i class="fa-solid fa-2x fa-n" style="color:#e50914"></i>',
+        "target" : "#video-list"
+    },
+    {
+        "title" : "Disney+",
+        "url" : "https://www.disneyplus.com/en-gb/home",
+        "icon" : '<i class="fa-solid fa-2x fa-hat-wizard" style="color:steelblue"></i>',
+        "target" : "#video-list"
+    },
+    {
+        "title" : "Prime Video",
+        "url" : "https://www.amazon.com/gp/video/storefront",
+        "icon" : '<i class="fa-brands fa-2x fa-amazon" style="color:#00a8e1;"></i>',
+        "target" : "#video-list"
+    },
+    {
+        "title" : "Gmail",
+        "url" : "https://mail.google.com/mail/u/0/#inbox",
+        "icon" : '<i class="fa-solid fa-2x fa-envelope" style="color:#ea4335"></i>',
+        "target" : "#tools-list"
+    },
+    {
+        "title" : "Google Drive",
+        "url" : "https://drive.google.com/drive/my-drive",
+        "icon" : '<i class="fa-brands fa-2x fa-google-drive" style="color:#fbbc04"></i>',
+        "target" : "#tools-list"
+    },
+    {
+        "title" : "Reddit",
+        "url" : "https://old.reddit.com",
+        "icon" : '<i class="fa-brands fa-2x fa-reddit" style="color:orangered"></i>',
+        "target" : "#tools-list"
+    },
+    {
+        "title" : "Recipe Book",
+        "url" : "https://erictilford.github.io/recipebook",
+        "icon" : '<i class="fa-solid fa-2x fa-book-skull" style="color:cornflowerblue"></i>',
+        "target" : "#tools-list"
+    },
+    {
+        "title" : "Wordle",
+        "url" : "https://www.nytimes.com/games/wordle/index.html",
+        "icon" : '<i class="fa-solid fa-2x fa-w" style="color:#69a662"></i>',
+        "target" : "#tools-list"
+    },
+    {
+        "title" : "Codepen",
+        "url" : "https://codepen.io/",
+        "icon" : '<i class="fa-brands fa-2x fa-codepen" style="color:#dddddd"></i>',
+        "target" : "#tools-list"
+    },
+    {
+        "title" : "Coolors",
+        "url" : "https://coolors.co/generate",
+        "icon" : '<i class="fa-solid fa-2x fa-swatchbook" style="color:lightseagreen"></i>',
+        "target" : "#tools-list"
+    },
+    {
+        "title" : "Pixlr X",
+        "url" : "https://pixlr.com/x/",
+        "icon" : '<i class="fa-solid fa-2x fa-palette" style="color:palevioletred"></i>',
+        "target" : "#tools-list"
+    },
+    // skribbl
+    // projects
+    // - home
+    // - wordle slackmojifier
+    // - scoreboard
+]
