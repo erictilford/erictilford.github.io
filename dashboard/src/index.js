6193d2b320a9ebdcec4b85b049ecb4d7f85ecbc3
Eric Tilford <eric.tilford@gmail.com>
2023-03-26 20:27:28 -0500

    added zip code API

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -132,120 +132,140 @@ $(document).ready(function () {
 
 	// WEATHER
 	// https://openweathermap.org/api/one-call-api
-	const wKey = config.WEATHER_API_KEY;
-	$.ajax({
-		url: "https://api.openweathermap.org/data/2.5/onecall?lat=35.55&lon=-97.61&lang=en&units=imperial&appid=" + wKey,
+	
+	let zipCode = 73132;
+	const lKey = config.LOCATION_API_KEY;
+	$.ajax({ // Get location data from Zip Code
+		url: "https://thezipcodes.com/api/v1/search?zipCode=" + zipCode + "&countryCode=US&apiKey=" + lKey,
 		type: "GET",
 		success: function (result) {
-			//console.log(result);
+			let city = result.location[0].city;
+			let state = result.location[0].stateCode2;
+			let lat = Math.round(result.location[0].latitude * 100) / 100;
+			let long = Math.round(result.location[0].longitude * 100) / 100;
 
-			function OneDec(x) { // round to one decimal place
-				return Math.round(x * 10) / 10
-			};
+			const wKey = config.WEATHER_API_KEY;
+			$.ajax({
+				url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&lang=en&units=imperial&appid=" + wKey,
+				type: "GET",
+				success: function (result) {
 
-			// Current temp
-			const temp = OneDec(result.current.temp);
-			const currentIcon = result.current.weather[0].icon;
-			$("#current-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + currentIcon + ".png");
-			$("#current-weather-icon").attr("title", result.current.weather[0].description);
-			$("#current-weather-text").text(temp + "°");
+					function OneDec(x) { // round to one decimal place
+						return Math.round(x * 10) / 10
+					};
 
-			const todayHighTemp = OneDec(result.daily[0].temp.max);
-			const todayLowTemp = OneDec(result.daily[0].temp.min);
-			const feelsLike = OneDec(result.current.feels_like);
-			const s =  /* todayHighTemp + "° / " + todayLowTemp + "°*/" (Feels like " + feelsLike + "°)";
-			$("#temp-text").text(s);
+					// Current temp
+					const temp = OneDec(result.current.temp);
+					const currentIcon = result.current.weather[0].icon;
+					$("#current-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + currentIcon + ".png");
+					$("#current-weather-icon").attr("title", result.current.weather[0].description);
+					$("#current-weather-text").text(temp + "°");
 
-			// Last updated text
-			let currentUTC = new Date(result.current.dt * 1000); // create a date from what the API provides
-			let pt = prettyTime(currentUTC);
-			$("#last-updated-text").text("Last Updated " + pt.time + pt.ampm);
+					const todayHighTemp = OneDec(result.daily[0].temp.max);
+					const todayLowTemp = OneDec(result.daily[0].temp.min);
+					const feelsLike = OneDec(result.current.feels_like);
+					const s =  /* todayHighTemp + "° / " + todayLowTemp + "°*/" (Feels like " + feelsLike + "°)";
+					$("#temp-text").text(s);
 
-			// Hourly
-			let hourlyColumns = "";
-			for (let i = 3; i < 18; i += 3){
-				let hourlyUTC = new Date(result.hourly[i].dt * 1000);
-				let pt = prettyTime(hourlyUTC);
-				let time = pt.hour + " " + pt.ampm;
-				let icon = "<img class='weather-icon' src='https://openweathermap.org/img/wn/" + result.hourly[i].weather[0].icon + ".png' title='" + result.hourly[i].weather[0].description + "'></img>";
-				let pop = "";
-				if (result.hourly[i].pop != 0) {
-					pop = "<i style='filter: opacity(" + (result.hourly[i].pop) + ");' class='fa-solid fa-droplet'></i> " + OneDec(result.hourly[i].pop * 100) + "%";
-				}
-				let temp = OneDec(result.hourly[i].temp) + "°";
-				hourlyColumns += "<td>" + time + "<br>" + icon + "<br>" + temp + "<br>" + pop + "</td>"; 
-			}
-			$("#weather-hourly-table").append("<tr>" + hourlyColumns + "</tr>");
-			
-			// 8-day Forecast
-			for (let i = 0; i < result.daily.length; i++) {
-				let dayName = "";
-				if (i == 0) {
-					dayName = "Today";
-				} else {
-					const thisDay = new Date(d);
-					thisDay.setDate(thisDay.getDate() + i);
-					const todayHolidays = checkHoliday(thisDay.getMonth() + 1, thisDay.getDate(), thisDay.getDay(), thisDay.getFullYear());
-					if (todayHolidays.length != 0) {
-						dayName = todayHolidays[0];
-					} else {
-						let j = i;
-						if (d.getDay() + i > 6) { j -= 7; }
-						dayName = dayNames[d.getDay() + j];
+					// Last updated text
+					let currentUTC = new Date(result.current.dt * 1000); // create a date from what the API provides
+					let pt = prettyTime(currentUTC);
+					$("#last-updated-text").text("Last Updated " + pt.time + pt.ampm);
+					//Location text
+					$("#location-text").text(city + ", " + state /* + " " + zipCode */);
+
+					// Hourly
+					let hourlyColumns = "";
+					for (let i = 3; i < 18; i += 3){
+						let hourlyUTC = new Date(result.hourly[i].dt * 1000);
+						let pt = prettyTime(hourlyUTC);
+						let time = pt.hour + " " + pt.ampm;
+						let icon = "<img class='weather-icon' src='https://openweathermap.org/img/wn/" + result.hourly[i].weather[0].icon + ".png' title='" + result.hourly[i].weather[0].description + "'></img>";
+						let pop = "";
+						if (result.hourly[i].pop != 0) {
+							pop = "<i style='filter: opacity(" + (result.hourly[i].pop) + ");' class='fa-solid fa-droplet'></i> " + OneDec(result.hourly[i].pop * 100) + "%";
+						}
+						let temp = OneDec(result.hourly[i].temp) + "°";
+						hourlyColumns += "<td>" + time + "<br>" + icon + "<br>" + temp + "<br>" + pop + "</td>"; 
 					}
+					$("#weather-hourly-table").append("<tr>" + hourlyColumns + "</tr>");
+					
+					// 8-day Forecast
+					for (let i = 0; i < result.daily.length; i++) {
+						let dayName = "";
+						if (i == 0) {
+							dayName = "Today";
+						} else {
+							const thisDay = new Date(d);
+							thisDay.setDate(thisDay.getDate() + i);
+							const todayHolidays = checkHoliday(thisDay.getMonth() + 1, thisDay.getDate(), thisDay.getDay(), thisDay.getFullYear());
+							if (todayHolidays.length != 0) {
+								dayName = todayHolidays[0];
+							} else {
+								let j = i;
+								if (d.getDay() + i > 6) { j -= 7; }
+								dayName = dayNames[d.getDay() + j];
+							}
+						}
+						const dayname = "<td class='day-name' valign='center'>" + dayName + "</td>";
+						const icon = "<td class='weather-icon-column'><img class='weather-icon' src='https://openweathermap.org/img/wn/" + result.daily[i].weather[0].icon + ".png' title='" + result.daily[i].weather[0].description + "'></td>";
+						const high = "<td class='daily-temp'>" + OneDec(result.daily[i].temp.max) + "°</td>";
+						const low = "<td class='daily-temp'>" + OneDec(result.daily[i].temp.min) + "°</td>";
+						let pop = "";
+						if (result.daily[i].pop == 0) {
+							pop = "<td class='precip'></td>";
+						} else {
+							pop = "<td class='precip'><i style='filter: opacity(" + (result.daily[i].pop) + ");' class='fa-solid fa-droplet'></i> " + OneDec(result.daily[i].pop * 100) + "%</td>";
+						}
+						const row = "<tr>" + dayname + pop + icon + high + low + "</tr>";
+						$("#weather-table").append(row);
+					}
+					
+					// UV index
+					const uvi = result.current.uvi;
+					let uvtext = "";
+					let uvcolor = "";
+					let uvicon = "fa-sun";
+					if (uvi == 0) { uvcolor = "aliceblue"; uvicon = "fa-moon" }
+					else if (uvi > 0 && uvi < 3) { uvtext = "Low | "; uvcolor = "khaki" }
+					else if (uvi >= 3 && uvi < 6) { uvtext = "Moderate | "; uvcolor = "goldenrod" }
+					else if (uvi >= 6 && uvi < 8) { uvtext = "High | "; uvcolor = "orangered" }
+					else if (uvi >= 8 && uvi < 11) { uvtext = "Very High | "; uvcolor = "crimson" }
+					else if (uvi >= 11) { uvtext = "Extreme | "; uvcolor = "magenta" }
+
+					$("#weather-extra-uv").html('<i class="fa-solid fa-lg ' + uvicon + ' weather-extra-icon" style="color:' + uvcolor + '"></i><br>UV Index<br>' + uvtext + result.current.uvi);
+					$("#weather-extra-wind").html('<i class="fa-solid fa-lg fa-wind weather-extra-icon" style="color:lightsteelblue"></i><br>Wind Speed<br>' + result.current.wind_speed + ' mph');
+					$("#weather-extra-humidity").html('<i class="fa-solid fa-lg fa-droplet weather-extra-icon" style="color:cornflowerblue"></i><br>Humidity<br>' + result.current.humidity + '%');
+
+					// Moon phases | moon.js
+					let moonPhase = result.daily[0].moon_phase;
+					$("#weather-extra-moon").html('<img src="assets/moonphases/' + moonPhaseInfo(moonPhase).image + '"><br>Moon Phase<br>' + moonPhaseInfo(moonPhase).text);
+
+					// Wind direction | wind.js
+					let windDeg = result.current.wind_deg;
+					let windDirectionString = '<div id="wind-deg-icon" style="margin-bottom:10px"><i class="fa-solid fa-lg fa-arrow-down-long weather-extra-icon" style="color:mediumslateblue; padding-bottom:0"></i></div>Wind Direction<br>';
+					$("#weather-extra-direction").html(windDirectionString  + windDirectionName(windDeg) + " | " + windDeg + "&deg; ");
+					$("#wind-deg-icon").css("transform","rotate(" + windDeg + "deg)");
+					
+					// Air pressure
+					let pressure = result.current.pressure;
+					let pressureName = "";
+					if (pressure > 1022.689) { pressureName = "High" }
+					else if (pressure <= 1022.689 && pressure > 1009.144) { pressureName = "Normal"}
+					else if (pressure <= 1009.144) { pressureName = "Low"}
+					$("#weather-extra-pressure").html('<i class="fa-solid fa-lg fa-gauge weather-extra-icon" style="color:burlywood"></i><br>Air Pressure<br>' + pressureName + ' | ' + pressure + " hPa");
+				},
+				error: function (error) {
+					console.log(error);
+					$("#temp-text").html("<p>Failed to Load Weather Data</p>");
 				}
-				const dayname = "<td class='day-name' valign='center'>" + dayName + "</td>";
-				const icon = "<td class='weather-icon-column'><img class='weather-icon' src='https://openweathermap.org/img/wn/" + result.daily[i].weather[0].icon + ".png' title='" + result.daily[i].weather[0].description + "'></td>";
-				const high = "<td class='daily-temp'>" + OneDec(result.daily[i].temp.max) + "°</td>";
-				const low = "<td class='daily-temp'>" + OneDec(result.daily[i].temp.min) + "°</td>";
-				let pop = "";
-				if (result.daily[i].pop == 0) {
-					pop = "<td class='precip'></td>";
-				} else {
-					pop = "<td class='precip'><i style='filter: opacity(" + (result.daily[i].pop) + ");' class='fa-solid fa-droplet'></i> " + OneDec(result.daily[i].pop * 100) + "%</td>";
-				}
-				const row = "<tr>" + dayname + pop + icon + high + low + "</tr>";
-				$("#weather-table").append(row);
-			}
-			
-			// UV index
-			const uvi = result.current.uvi;
-			let uvtext = "";
-			let uvcolor = "";
-			let uvicon = "fa-sun";
-			if (uvi == 0) { uvcolor = "aliceblue"; uvicon = "fa-moon" }
-			else if (uvi > 0 && uvi < 3) { uvtext = "Low | "; uvcolor = "khaki" }
-			else if (uvi >= 3 && uvi < 6) { uvtext = "Moderate | "; uvcolor = "goldenrod" }
-			else if (uvi >= 6 && uvi < 8) { uvtext = "High | "; uvcolor = "orangered" }
-			else if (uvi >= 8 && uvi < 11) { uvtext = "Very High | "; uvcolor = "crimson" }
-			else if (uvi >= 11) { uvtext = "Extreme | "; uvcolor = "magenta" }
-
-			$("#weather-extra-uv").html('<i class="fa-solid fa-lg ' + uvicon + ' weather-extra-icon" style="color:' + uvcolor + '"></i><br>UV Index<br>' + uvtext + result.current.uvi);
-			$("#weather-extra-wind").html('<i class="fa-solid fa-lg fa-wind weather-extra-icon" style="color:lightsteelblue"></i><br>Wind Speed<br>' + result.current.wind_speed + ' mph');
-			$("#weather-extra-humidity").html('<i class="fa-solid fa-lg fa-droplet weather-extra-icon" style="color:cornflowerblue"></i><br>Humidity<br>' + result.current.humidity + '%');
-
-			// Moon phases | moon.js
-			let moonPhase = result.daily[0].moon_phase;
-			$("#weather-extra-moon").html('<img src="assets/moonphases/' + moonPhaseInfo(moonPhase).image + '"><br>Moon Phase<br>' + moonPhaseInfo(moonPhase).text);
-
-			// Wind direction | wind.js
-			let windDeg = result.current.wind_deg;
-			let windDirectionString = '<div id="wind-deg-icon" style="margin-bottom:10px"><i class="fa-solid fa-lg fa-arrow-down-long weather-extra-icon" style="color:mediumslateblue; padding-bottom:0"></i></div>Wind Direction<br>';
-			$("#weather-extra-direction").html(windDirectionString  + windDirectionName(windDeg) + " | " + windDeg + "&deg; ");
-			$("#wind-deg-icon").css("transform","rotate(" + windDeg + "deg)");
-			
-			// Air pressure
-			let pressure = result.current.pressure;
-			let pressureName = "";
-			if (pressure > 1022.689) { pressureName = "High" }
-			else if (pressure <= 1022.689 && pressure > 1009.144) { pressureName = "Normal"}
-			else if (pressure <= 1009.144) { pressureName = "Low"}
-			$("#weather-extra-pressure").html('<i class="fa-solid fa-lg fa-gauge weather-extra-icon" style="color:burlywood"></i><br>Air Pressure<br>' + pressureName + ' | ' + pressure + " hPa");
+			});
 		},
 		error: function (error) {
 			console.log(error);
-			$("#temp-text").html("<p>Failed to Load Weather Data</p>");
+			$("#temp-text").html("<p>Failed to Load Location Data</p>");
 		}
+		
 	});
 	// YYYY/MM/DD Format
 	//var output = d.getFullYear() + '/' +
