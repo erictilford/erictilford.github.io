6706c309e0ad2d166e7aa2d133f4403b3a4c8901
Eric <erictilford@gmail.com>
2023-11-29 00:22:06 -0600

    big update!

--- chrome/content.js
+++ chrome/content.js
@@ -1,67 +1,74 @@
-console.log("Content Script Loaded");
+if (window.location.href.includes("toonamiaftermath.com/")) {
+  console.log("You are on TOONAMI FUCKING AFTERMATH!");
 
-function setContainerStyles() {
-    var viewContainer = document.querySelector(".view-container");
-    if (viewContainer) {
-        viewContainer.style.height = "calc(100vh - 160px)";
-        viewContainer.style.backgroundColor = "";
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+
+
+      function checkForToonamiElement() {
+        // check if DOM elements exist
+        var element = document.querySelector(".view-container");
+
+        if (element) {
+          setToonamiContainerStyles();
+          // Stop checking by clearing the interval
+          clearInterval(intervalId);
+        }
+      }
+
+      // Set up the interval to call the function every x ms
+      var intervalId = setInterval(checkForToonamiElement, 100);
+
+      function checkForTheaterButton() {
+        var theaterButton = document.querySelector("button.theater");
+        if (theaterButton) {
+          theaterButton.addEventListener('click', function () {
+
+            setTimeout(function () {
+              setToonamiContainerStyles()
+            }, 100);
+
+          });
+          clearInterval(intervalId2);
+        }
+      }
+
+      var intervalId2 = setInterval(checkForTheaterButton, 100);
+
+      function setToonamiContainerStyles() {
+        var viewContainer = document.querySelector(".view-container");
+        var theaterModeContainer = document.querySelector(".theaterMode .view-container");
+        viewContainer.style.backgroundColor = "transparent";
+        if (theaterModeContainer) {
+
+          console.log("theater mode");
+          viewContainer.style.height = "100%";
+          viewContainer.style.backgroundColor = "transparent";
+        }
+        else if (viewContainer) {
+
+          console.log("not theater mode");
+          viewContainer.style.height = "calc(100vh - 160px)";
+          viewContainer.style.backgroundColor = "black";
+        }
+
+        var videoContainer = document.querySelector(".video-container");
+        if (videoContainer) {
+          videoContainer.style.height = "100%";
+          videoContainer.style.width = "100%";
+        }
+
+        var footerContainer = document.querySelector(".content");
+        if (footerContainer) { footerContainer.remove() }
+
+        var nav = document.querySelector("nav");
+        if (nav) { nav.remove() };
+
+      }
+
     }
-
-    var videoContainer = document.querySelector(".video-container");
-    if (videoContainer) {
-        videoContainer.style.height = "100%";
-        videoContainer.style.width = "100%";
-    }
-
-    var footerContainer = document.querySelector(".content");
-    if (footerContainer) { footerContainer.remove() }
-
-    var nav = document.querySelector("nav");
-    if (nav) { nav.remove() };
-
+  };
 }
 
-setContainerStyles();
 
-window.addEventListener("resize", setContainerStyles);
 
-var intervalId = setInterval(setContainerStyles, 1000);
-
-// Set a timeout to clear the interval after 10 seconds
-setTimeout(function() {
-  clearInterval(intervalId);
-  console.log("Interval cleared after 10 seconds");
-}, 10000);
-
-/*
-
-if (window.location.href.includes("https://www.toonamiaftermath.com/")) {
-  alert("You are on TOONAMI FUCKING AFTERMATH!");
-
-  function setContainerStyles() {
-      var viewContainer = document.querySelector(".view-container");
-      if (viewContainer) {
-        viewContainer.style.height = "calc(100vh - 410px)";
-        viewContainer.style.backgroundColor = "";
-      }
-  
-      var videoContainer = document.querySelector(".video-container");
-      if (videoContainer) {
-        videoContainer.style.height = "100%";
-        videoContainer.style.width = "100%";
-      }
-      
-      var footerContainer = document.querySelector(".content");
-      if (footerContainer) { footerContainer.remove() }
-      
-      var nav = document.querySelector("nav");
-      if (nav) { nav.remove() };
-   
-   }
-  
-    setContainerStyles();
-  
-    window.addEventListener("resize", setContainerStyles);
-}
-
-*/
