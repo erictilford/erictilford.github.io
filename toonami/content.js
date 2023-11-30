592db90699bbf5fa4fe5793e0ea8ccb4ce87719e
Eric Tilford <eric.tilford@tylertech.com>
2023-11-30 15:14:07 -0600

    trying to add settings

--- toonami/content.js
+++ toonami/content.js
@@ -1,74 +1,82 @@
-if (window.location.href.includes("toonamiaftermath.com/")) {
-  console.log("You are on TOONAMI FUCKING AFTERMATH!");
-
-  document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
 
 
-      function checkForToonamiElement() {
-        // check if DOM elements exist
-        var element = document.querySelector(".view-container");
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
 
-        if (element) {
-          setToonamiContainerStyles();
-          // Stop checking by clearing the interval
-          clearInterval(intervalId);
-        }
+
+
+
+
+
+    function checkForToonamiElement() {
+      // check if DOM elements exist
+      var element = document.querySelector(".view-container");
+
+      if (element) {
+        setToonamiContainerStyles();
+        // Stop checking by clearing the interval
+        clearInterval(intervalId);
       }
+    }
 
-      // Set up the interval to call the function every x ms
-      var intervalId = setInterval(checkForToonamiElement, 100);
+    // Set up the interval to call the function every x ms
+    var intervalId = setInterval(checkForToonamiElement, 100);
 
-      function checkForTheaterButton() {
-        var theaterButton = document.querySelector("button.theater");
-        if (theaterButton) {
-          theaterButton.addEventListener('click', function () {
+    function checkForTheaterButton() {
+      var theaterButton = document.querySelector("button.theater");
+      if (theaterButton) {
+        theaterButton.addEventListener('click', function () {
 
-            setTimeout(function () {
-              setToonamiContainerStyles()
-            }, 100);
+          setTimeout(function () {
+            setToonamiContainerStyles()
+          }, 100);
 
-          });
-          clearInterval(intervalId2);
-        }
+        });
+        clearInterval(intervalId2);
       }
+    }
 
-      var intervalId2 = setInterval(checkForTheaterButton, 100);
+    var intervalId2 = setInterval(checkForTheaterButton, 100);
 
-      function setToonamiContainerStyles() {
-        var viewContainer = document.querySelector(".view-container");
-        var theaterModeContainer = document.querySelector(".theaterMode .view-container");
+    function setToonamiContainerStyles() {
+
+
+
+      
+      var viewContainer = document.querySelector(".view-container");
+      var theaterModeContainer = document.querySelector(".theaterMode .view-container");
+      viewContainer.style.backgroundColor = "transparent";
+      if (theaterModeContainer) {
+
+        //console.log("theater mode");
+        viewContainer.style.height = "100%";
         viewContainer.style.backgroundColor = "transparent";
-        if (theaterModeContainer) {
-
-          console.log("theater mode");
-          viewContainer.style.height = "100%";
-          viewContainer.style.backgroundColor = "transparent";
-        }
-        else if (viewContainer) {
-
-          console.log("not theater mode");
-          viewContainer.style.height = "calc(100vh - 160px)";
-          viewContainer.style.backgroundColor = "black";
-        }
-
-        var videoContainer = document.querySelector(".video-container");
-        if (videoContainer) {
-          videoContainer.style.height = "100%";
-          videoContainer.style.width = "100%";
-        }
-
-        var footerContainer = document.querySelector(".content");
-        if (footerContainer) { footerContainer.remove() }
-
-        var nav = document.querySelector("nav");
-        if (nav) { nav.remove() };
-
       }
+      else if (viewContainer) {
+
+    
+
+        //console.log("not theater mode");
+        viewContainer.style.height = "calc(100vh - 160px)";
+        viewContainer.style.backgroundColor = "black";
+      }
+
+      var videoContainer = document.querySelector(".video-container");
+      if (videoContainer) {
+        videoContainer.style.height = "100%";
+        videoContainer.style.width = "100%";
+      }
+
+      var footerContainer = document.querySelector(".content");
+      if (footerContainer) { footerContainer.remove() }
+
+      var nav = document.querySelector("nav");
+      if (nav) { nav.remove() };
 
     }
-  };
-}
+
+  }
+};
 
 
 
--- toonami/popup.js
+++ toonami/popup.js
@@ -1,3 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    //alert("HELLO");
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Retrieve the stored checkbox states on popup load
+  chrome.storage.sync.get({ showNav: false, showFooter: false }, function (data) {
+    var showNavCheckbox = document.getElementById('showNav');
+    var showFooterCheckbox = document.getElementById('showFooter');
+
+    showNavCheckbox.checked = data.showNav;
+    showFooterCheckbox.checked = data.showFooter;
   });
+
+  // Add event listeners to the checkboxes
+  document.querySelectorAll('.featureCheckbox').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+      // Save the checkbox states to storage
+      var showNavState = document.getElementById('showNav').checked;
+      var showFooterState = document.getElementById('showFooter').checked;
+      chrome.storage.sync.set({ showNav: showNavState, showFooter: showFooterState });
+
+      // Send a message to content script when the checkbox states change
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { showNav: showNavState, showFooter: showFooterState });
+      });
+    });
+  });
+  
+});
