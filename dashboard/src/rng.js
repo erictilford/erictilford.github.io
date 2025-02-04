e2cc007f36a812d517566f9ac077b3d53eb11b3d
Eric Tilford <eric.tilford@tylertech.com>
2025-02-04 15:50:27 -0600

    .

---
+++ dashboard/src/rng.js
@@ -0,0 +1,17 @@
+function LoadRNG(animSpeed) {
+    icon = 'fa-solid fa-dice " style="color:darkcyan';
+			$("#rng-title").append(' <i class=" ' + icon + '">');
+			$("#widget-list").append('<a id="rng-button" class="pointer"><li><i class="fa-2x ' + icon + '"></i><br>RNG</li></a>');
+			// $("#close-rng-button").click(function() { $("#rng-panel").hide(animSpeed); });
+			$("#rng-button").click(function() {  
+                $("#rng-panel").toggle(animSpeed); 
+                $('html,body').animate({
+                    scrollTop: $("#rng-panel").offset().top
+                 });
+            });
+			$("#rng-button").attr("title",  "Random Number Generator" );
+
+            $("#d6-button").append('<i class="fa-solid fa-dice-six fa-3x">');
+            $("#d20-button").append('<i class="fa-solid fa-dice-d20 fa-3x">');
+
+}
