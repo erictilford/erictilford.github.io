36ea30f8c6db818191975b03e3855722aae66317
Eric Tilford <eric.tilford@tylertech.com>
2025-08-13 09:38:49 -0500

    ?

--- tpe-exploder/content.js
+++ tpe-exploder/content.js
@@ -3,8 +3,35 @@ document.onreadystatechange = () => {
 
     var elements = document.querySelectorAll('.ui-accordion .ui-accordion-content');
     elements.forEach(function (element) {
-      element.style.height = 'auto'; element.style.maxHeight = '700px'; element.style.padding = '10px';
+      element.style.height = 'auto';
+      element.style.maxHeight = '700px';
+      element.style.padding = '10px';
     });
+
+    // Timer logic
+    let startSeconds = 15 * 60;
+    let seconds = startSeconds;
+    const originalTitle = document.title;
+
+    function pad(n) {
+      return n < 10 ? '0' + n : n;
+    }
+
+    function updateTitle() {
+      const min = Math.floor(Math.abs(seconds) / 60);
+      const sec = Math.abs(seconds) % 60;
+      const sign = seconds < 0 ? '-' : '';
+      document.title = `${sign}${pad(min)}:${pad(sec)} ${originalTitle}`;
+      console.log("WTF");
+    }
+
+    updateTitle();
+    setInterval(() => {
+      seconds--;
+      updateTitle();
+    }, 1000);
+
+    
 
   }
 };
