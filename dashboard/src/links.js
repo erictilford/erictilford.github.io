036292e0730cbfa9683b7503d8d376545fad840e
Eric <erictilford@gmail.com>
2026-01-26 20:52:56 -0600

    cold weather

--- dashboard/src/links.js
+++ dashboard/src/links.js
@@ -6,192 +6,223 @@ const links = [
         "title" : "Toonami",
         "url" : "https://www.toonamiaftermath.com",
         "icon" : `<i class="fa-solid ${iconSize} fa-meteor" style="color:mediumaquamarine"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "Twitch",
         "url" : "https://www.twitch.tv/directory/following/live",
         "icon" : `<i class="fa-brands ${iconSize} fa-twitch" style="color:#a970ff"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "YouTube",
         "url" : "https://www.youtube.com",
         "icon" : `<i class="fa-brands ${iconSize} fa-youtube" style="color:red;"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "Netflix",
         "url" : "https://www.netflix.com",
         "icon" : `<i class="fa-solid ${iconSize} fa-n" style="color:#e50914"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "Disney+",
         "url" : "https://www.disneyplus.com/en-gb/home",
         "icon" : `<i class="fa-solid ${iconSize} fa-hat-wizard" style="color:steelblue"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "Prime Video",
         "url" : "https://www.amazon.com/Amazon-Video/b/?ie=UTF8&node=2858778011&ref_=nav_cs_prime_video",
         "icon" : `<i class="fa-brands ${iconSize} fa-amazon" style="color:#00a8e1;"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     /*
     {
         "title" : "Multistre.am",
         "url" : "https://multistre.am/",
         "icon" : `<i class="fa-solid ${iconSize} fa-box fa-rotate-180" style="color:#1968aa;"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "LazyGoat",
         "url" : "https://lazygoat.tv/collection/y2sk7-nrAgbzRVHhFZUsyMEpvJEd/",
         "icon" : `<i class="fa-solid ${iconSize} fa-table-cells-large" style="color:#FD5205;"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     */
     {
         "title" : "Adult Swim",
         "url" : "https://www.adultswim.com/streams/",
         "icon" : `<i class="fa-solid ${iconSize} fa-water-ladder" style="color:#eeeeee;"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     /*
     {
         "title" : "Crunchyroll",
         "url" : "https://www.crunchyroll.com/",
         "icon" : `<i class="fa-solid ${iconSize} fa-eye" style="color:#f47521;"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     */
     {
         "title" : "Gmail",
         "url" : "https://mail.google.com/mail/u/0/#inbox",
         "icon" : `<i class="fa-solid ${iconSize} fa-envelope" style="color:#ea4335"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     {
         "title" : "Drive",
         "url" : "https://drive.google.com/drive/my-drive",
         "icon" : `<i class="fa-brands ${iconSize} fa-google-drive" style="color:#fbbc04"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     {
         "title" : "Reddit",
         "url" : "https://old.reddit.com",
         "icon" : `<i class="fa-brands ${iconSize} fa-reddit-alien" style="color:orangered"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     {
         "title" : "Codepen",
         "url" : "https://codepen.io/",
         "icon" : `<i class="fa-brands ${iconSize} fa-codepen" style="color:#dddddd"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     /*{
         "title" : "Coolors",
         "url" : "https://coolors.co/generate",
         "icon" : `<i class="fa-solid ${iconSize} fa-swatchbook" style="color:lightseagreen"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },*/
     /*{
         "title" : "Pixlr X",
         "url" : "https://pixlr.com/x/",
         "icon" : `<i class="fa-solid ${iconSize} fa-palette" style="color:bisque"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },*/
     {
         "title" : "Codewars",
         "url" : "https://www.codewars.com/dashboard",
         "icon" : `<i class="fa-solid ${iconSize} fa-laptop-code" style="color:#b1361e"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     {
         "title" : "Duolingo",
         "url" : "https://www.duolingo.com/",
         "icon" : `<i class="fa-solid ${iconSize} fa-feather" style="color:#54ae43"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     {
         "title" : "Scryfall",
         "url" : "https://scryfall.com/",
         "icon" : `<i class="fa-solid ${iconSize} fa-id-card" style="color:#a04b9a"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     {
         "title" : "ChatGPT",
         "url" : "https://chat.openai.com/",
         "icon" : `<i class="fa-regular ${iconSize} fa-comments" style="color:#71a699"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     {
         "title" : "Font Awesome",
         "url" : "https://fontawesome.com/search?o=r&m=free",
         "icon" : `<i class="fa-solid ${iconSize} fa-font-awesome" style="color:#528dd7"></i>`,
-        "target" : "#tools-list"
+        "target" : "#tools-list",
+        "private" : false
     },
     {
         "title" : "Timeguessr",
         "url" : "https://timeguessr.com/",
         "icon" : `<i class="fa-solid ${iconSize} fa-map-location-dot" style="color:#db5049"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     {
         "title" : "Wordle",
         "url" : "https://www.nytimes.com/games/wordle/index.html",
         "icon" : `<i class="fa-solid ${iconSize} fa-w" style="color:#69a662"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     {
         "title" : "Connections",
         "url" : "https://www.nytimes.com/games/connections",
         "icon" : `<i class="fa-solid ${iconSize} fa-table-cells" style="color:#bc70c4"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     {
         "title" : "Mini-Cross",
         "url" : "https://www.seattletimes.com/games-crossword-mini/",
         "icon" : `<i class="fa-solid ${iconSize} fa-puzzle-piece" style="color:#6493e6"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     {
         "title" : "Bandle",
         "url" : "https://bandle.app/daily",
         "icon" : `<i class="fa-solid ${iconSize} fa-guitar" style="color:#981520"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     {
         "title" : "Catfishing",
         "url" : "https://catfishing.net/",
         "icon" : `<i class="fa-solid ${iconSize} fa-fish-fins" style="color:#fbbf24"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     {
         "title" : "Scrandle",
         "url" : "https://scrandle.com/",
         "icon" : `<i class="fa-solid ${iconSize} fa-hotdog" style="color:#ce9864"></i>`,
-        "target" : "#games-list"
+        "target" : "#games-list",
+        "private" : false
     },
     // Audio
     {
         "title" : "NPR",
         "url" : "https://www.npr.org",
         "icon" : `<i class="fa-solid ${iconSize} fa-microphone-lines" style="color:lightslategray"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "Classical",
         "url" : "https://theclassicalstation.org/listen/",
         "icon" : `<i class="fa-solid ${iconSize} fa-music" style="color:sandybrown"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     {
         "title" : "Work Music",
         "url" : "https://www.youtube.com/playlist?list=PLWx-JvCYWQuE1BfLAhIaVMCK4GrVYRotC",
         "icon" : `<i class="fa-brands ${iconSize} fa-youtube-square" style="color:#cb0e0e"></i>`,
-        "target" : "#video-list"
+        "target" : "#video-list",
+        "private" : false
     },
     // Projects
     /*
@@ -199,40 +230,46 @@ const links = [
         "title" : "Home",
         "url" : "https://erictilford.github.io",
         "icon" : `<i class="fa-solid ${iconSize} fa-house-chimney-user" style="color:lightblue"></i>`,
-        "target" : "#project-list"
+        "target" : "#project-list",
+        "private" : false
     },
     */
     {
         "title" : "Github",
         "url" : "https://github.com/erictilford/erictilford.github.io",
         "icon" : `<i class="fa-brands ${iconSize} fa-github" style="color:slategray"></i>`,
-        "target" : "#project-list"
+        "target" : "#project-list",
+        "private" : false
     },
     {
         "title" : "Recipe Book",
         "url" : "https://erictilford.github.io/recipebook",
         "icon" : `<i class="fa-solid ${iconSize} fa-book-skull" style="color:cornflowerblue"></i>`,
-        "target" : "#project-list"
+        "target" : "#project-list",
+        "private" : false
     },
     /*
     {
         "title" : "Wellness & Longevity",
         "url" : "https://wellnessok.com",
         "icon" : `<i class="fa-solid ${iconSize} fa-syringe" style="color:#85aeb5"></i>`,
-        "target" : "#project-list"
+        "target" : "#project-list",
+        "private" : false
     },
     */
     {
         "title" : "Wordle Slackmojifier",
         "url" : "https://erictilford.github.io/wordle",
         "icon" : `<i class="fa-solid ${iconSize} fa-icons" style="color:orchid"></i>`,
-        "target" : "#project-list"
+        "target" : "#project-list",
+        "private" : false
     },
     {
         "title" : "PW Generator",
         "url" : "https://erictilford.github.io/password",
         "icon" : `<i class="fa-solid ${iconSize} fa-key" style="color:#f7c964"></i>`,
-        "target" : "#project-list"
+        "target" : "#project-list",
+        "private" : false
     },
     // +Password Generator?
 ];
