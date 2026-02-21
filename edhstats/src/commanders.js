2727dc23dd6fb16ffd17e4614b0d9615e045dd36
Eric <erictilford@gmail.com>
2026-02-21 15:16:06 -0600

    edhstats

---
+++ edhstats/src/commanders.js
@@ -0,0 +1,142 @@
+const headers = ["Commander", "EDHREC Rank", "Owner", "Type", "Status"];
+
+const commanders = [
+    // ERIC
+    {
+        "commander": "Chatterfang, Squirrel General",
+        "edhrecrank": 24,
+        "owner": "Eric",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Kuja, Genome Sorcerer",
+        "edhrecrank": 100,
+        "owner": "Eric",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Omnath, Locus of Mana",
+        "edhrecrank": 220,
+        "owner": "Eric",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Zul Ashur, Lich Lord",
+        "edhrecrank": 900,
+        "owner": "Eric",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Dhalsim, Pliable Pacifist",
+        "edhrecrank": 1001,
+        "owner": "Eric",
+        "type": "Constructed",
+        "status": "Unowned"
+    },
+    {
+        "commander": "Miles \"Tails\" Prower",
+        "edhrecrank": 1047,
+        "owner": "Eric",
+        "type": "Constructed",
+        "status": "Unowned"
+    },
+    {
+        "commander": "Kibo, Uktabi Prince",
+        "edhrecrank": 132,
+        "owner": "Eric",
+        "type": "Constructed",
+        "status": "Unowned"
+    },
+    {
+        "commander": "Hearthhull, the Worldseed",
+        "edhrecrank": 51,
+        "owner": "Eric",
+        "type": "Unmodified Precon",
+        "status": "Active"
+    },
+
+    // JUSTIN
+    {
+        "commander": "Kangee, Sky Warden",
+        "edhrecrank": 971,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Aboshan, Cephalid Emperor",
+        "edhrecrank": 2178,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Eluge, the Shoreless Sea",
+        "edhrecrank": 142,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Uril, The Miststalker",
+        "edhrecrank": 457,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Sonic the Hedgehog",
+        "edhrecrank": 254,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Unowned"
+    },
+    {
+        "commander": "Zangief, the Red Cyclone",
+        "edhrecrank": 372,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Unowned"
+    },
+    {
+        "commander": "The Master of Keys",
+        "edhrecrank": 306,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Unowned"
+    },
+    {
+        "commander": "Elesh Norn, Mother of Machines",
+        "edhrecrank": 312,
+        "owner": "Justin",
+        "type": "Constructed",
+        "status": "Unowned"
+    },
+
+    // CAL
+    {
+        "commander": "Umbris, Fear Manifest",
+        "edhrecrank": 209,
+        "owner": "Cal",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Katara, the Fearless",
+        "edhrecrank": 415,
+        "owner": "Cal",
+        "type": "Constructed",
+        "status": "Active"
+    },
+    {
+        "commander": "Bre of Clan Stoutarm",
+        "edhrecrank": 705,
+        "owner": "Cal",
+        "type": "Constructed",
+        "status": "Active"
+    }
+];
---
+++ edhstats/src/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    // Add headers to the thead of #myTable
+    let thead = $("#myTable thead");
+    let headerRow = "<tr>";
+
+    headers.forEach(header => {
+        headerRow += `<th>${header}</th>`;
+    });
+
+    headerRow += "</tr>";
+    thead.append(headerRow);
+
+    // Add rows to the tbody of #myTable
+    let tbody = $("#myTable tbody");
+    commanders.forEach(commander => {
+        let row = "<tr>";
+        row += `<td>${commander.commander}</td>`;
+        row += `<td>${commander.edhrecrank}</td>`;
+        row += `<td>${commander.owner}</td>`;
+        row += `<td>${commander.type}</td>`;
+        row += `<td>${commander.status}</td>`;
+        row += "</tr>";
+        tbody.append(row);
+    });
+
+    // Initialize DataTable
+    let table = new DataTable('#myTable');
+});
